9320477e54679890fb5f449ed3080e44f3025286
Kris De Volder <kdevolder@pivotal.io>
2015-09-22 12:19:48 -0700

    Remove some remnants of V1 message sorting that where missed

--- cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/rest/LoggregatorMessageHandler.java
+++ cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/rest/LoggregatorMessageHandler.java
@@ -3,7 +3,6 @@ package org.cloudfoundry.client.lib.rest;
 import javax.websocket.MessageHandler;
 
 import org.cloudfoundry.client.lib.ApplicationLogListener;
-import org.cloudfoundry.client.lib.util.MessageSorter;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 
@@ -11,18 +10,16 @@ public class LoggregatorMessageHandler implements MessageHandler.Whole<byte[]> {
 
 	private final LoggregatorMessageParser messageParser;
 	private final ApplicationLogListener listener;
-	private final MessageSorter messageSorter;
 
 	public LoggregatorMessageHandler(ApplicationLogListener listener) {
 		this.listener = listener;
 		this.messageParser = new LoggregatorMessageParser();
-		this.messageSorter = new MessageSorter(listener);
 	}
 
 	public void onMessage(byte[] rawMessage) {
 		try {
 			messageParser.parseMessage(rawMessage);
-			messageSorter.onMessage(messageParser.parseMessage(rawMessage));
+			listener.onMessage(messageParser.parseMessage(rawMessage));
 		} catch (InvalidProtocolBufferException e) {
 			listener.onError(e);
 		}
